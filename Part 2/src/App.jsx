1c371eb69ddb730e7fad5e9814a420ded6004245
hfbcgg <150598647+hfbcgg@users.noreply.github.com>
2026-01-08 14:27:28 +0100

    Part 2

---
+++ Part 2/src/App.jsx
@@ -0,0 +1,82 @@
+// const Course = ({course}) => {
+
+import Course from "./components/Course"
+
+
+// const total = course.parts.reduce((s, p) => {return s+p.exercises}, 0);
+ 
+//   return(
+//     <div>
+//       <h1>{course.name}</h1>
+//       {course.parts.map(part => 
+//         <Part key={part.id} part={part}/>
+//         )}
+//       ilosc wszystkich zadan to {total}
+//     </div>
+//   )
+// }
+// const Part = ({part}) => {
+//   return(
+//     <p>
+//       {part.name} {part.exercises}
+//     </p>
+//   )
+// }
+
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      {courses.map(course => 
+        <Course key={course.id} course={course}/>
+        )}
+    </div>
+  )
+}
+
+export default App
