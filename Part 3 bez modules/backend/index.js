35dae7c6fcb5c9db8dc5159831c4d310c7aeb358
Lukasz M <funchfunch2@gmail.com>
2026-04-09 14:51:27 +0200

    usuniecie corsa

--- Part 3 bez modules/backend/index.js
+++ Part 3 bez modules/backend/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = '/api/notes'
 const app = express()
 app.use(express.static('dist'))
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
 
 let persons = [
     { 
